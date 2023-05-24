788208f3b865d356c25ad1fddce6cf0a687cd885
Harris Grant <harris@shoptreen.com>
2023-05-24 15:00:32 +0200

    added index view and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/restaurants/', to: 'restaurants#index', as: 'restaurants'
+  get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+roots_and_rolls = {name: "Roots & Rolls", address: "C/ del Consell de Cent, 401, bajos, 08009 derecha, Barcelona", category: "japanese", phone_number: "931 71 79 02"}
+gallo_santo = {name: "Gallo Santo", address: "Carrer del Torrent de l'Olla, 64, 08012 Barcelona", category: "french", phone_number: "930 27 67 46"}
+honest_greens = {name: "Honest Greens", address: "Rambla de Catalunya, 3, 08007 Barcelona", category: "italian", phone_number: "931 22 76 64"}
+flax_and_kale = {name: "Flax & Kale", address: "Pg. de Gràcia, 11, 08002 Barcelona", category: "chinese", phone_number: "931 44 08 12"}
+exotic_brunch = {name: "Exotic Brunch", address: "Carrer de Calàbria, 204, 08029 Barcelona", category: "belgian", phone_number: "605 45 05 59"}
+
+[roots_and_rolls, gallo_santo, honest_greens, flax_and_kale, exotic_brunch]. each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
