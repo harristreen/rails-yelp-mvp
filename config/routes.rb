14d2e1764a2fbb795350fc5e24130f2595cc6015
Harris Grant <harris@shoptreen.com>
2023-05-24 18:18:47 +0200

    completed restaurants and review model, added review controller and create/new view for review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get '/restaurants/', to: 'restaurants#index', as: 'restaurants'
+  get '/restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
   get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  get '/restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post '/restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
 end
